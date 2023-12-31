78c74a7e7835533942536f786f74afbec9eaea49
JorgeRight34 <jorgelright34rd30012003@gmail.com>
2023-12-31 10:58:42 -0400

    Migratio problem solved, solution: add migrate=Migrate(app,db) in the app factory

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from app import create_app, db
 from app.models import User, Post, File, Follow, Comment
 from flask_migrate import Migrate
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+if __name__=='__main__':
+    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    migrate = Migrate(app, db)
+    app.run(debug=True)
 
 @app.shell_context_processor
 def make_shell_context():
